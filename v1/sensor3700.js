596d35ff492d8d988b061057384a3b417666d685
Tomas Markevičius <tomas.m@8devices.com>
2018-02-21 13:39:18 +0000

    Rename and modify 3700 sensor node files

---
+++ v1/sensor3700.js
@@ -0,0 +1,120 @@
+'use strict';
+
+const restAPI = require('restserver-api');
+
+module.exports = function (RED) {
+  function SensorNode(config) {
+    RED.nodes.createNode(this, config);
+    const node = this;
+    node.service = RED.nodes.getNode(config.service);
+
+    node.powerSourceVoltage = config.powerSourceVoltage ;
+    node.activePower = config.activePower;
+    node.activeEnergy = config.activeEnergy;
+    node.reactivePower = config.reactivePower;
+    node.reactiveEnergy = config.reactiveEnergy;
+    node.relay = config.relay;
+    node.observe_time = config.interval;
+    node.name = config.uuid;
+    node.paths = [];
+    node.device = new restAPI.Device(node.service.service, node.name);
+
+    node.on('input', (msg) => {
+      node.device.getObjects().then((data) => {
+        node.send(data);
+      }).catch((err) => {
+        msg.error = err;
+        node.send(msg);
+      });
+    });
+
+    configure() {
+      if (node.powerSourceVoltage) {
+        //node.paths.push('/3/0/7');
+        node.device.observe('/3/0/7', (err, resp) => {
+          let msg = {};
+          msg.response = resp;
+          msg.title = "observation response";
+          const buf = Buffer.from(resp, 'base64');
+          let state = buf[3]; // TODO: parse TLV
+          msg.payload = state;
+          node.error(err);
+          node.send(msg);
+        });
+      }
+
+      if (node.activePower) {
+        //node.paths.push('/3305/0/5800');
+        node.device.observe('/3305/0/5800', (err, resp) => {
+          let msg = {};
+          msg.response = resp;
+          msg.title = "observation response";
+          const buf = Buffer.from(resp, 'base64');
+          let state = buf[3]; // TODO: parse TLV
+          msg.payload = state;
+          node.error(err);
+          node.send(msg);
+        });
+      }
+
+      if (node.activeEnergy) {
+        // node.paths.push('/3305/0/5805');
+        node.device.observe('/3305/0/5805', (err, resp) => {
+          let msg = {};
+          msg.response = resp;
+          msg.title = "observation response";
+          const buf = Buffer.from(resp, 'base64');
+          let state = buf[3]; // TODO: parse TLV
+          msg.payload = state;
+          node.error(err);
+          node.send(msg);
+        });
+      }
+
+      if (node.reactivePower) {
+        // node.paths.push('/3305/0/5815');
+        node.device.observe('/3305/0/5815', (err, resp) => {
+          let msg = {};
+          msg.response = resp;
+          msg.title = "observation response";
+          const buf = Buffer.from(resp, 'base64');
+          let state = buf[3]; // TODO: parse TLV
+          msg.payload = state;
+          node.error(err);
+          node.send(msg);
+        });
+      }
+
+      if (node.reactiveEnergy) {
+        // node.paths.push('/3305/0/5810');
+        node.device.observe('/3305/0/5810', (err, resp) => {
+          let msg = {};
+          msg.response = resp;
+          msg.title = "observation response";
+          const buf = Buffer.from(resp, 'base64');
+          let state = buf[3]; // TODO: parse TLV
+          msg.payload = state;
+          node.error(err);
+          node.send(msg);
+        });
+      }
+      
+      if (node.relay) {
+        // node.paths.push('/3312/0/5850');
+        node.device.observe('/3312/0/5850', (err, resp) => {
+          let msg = {};
+          msg.response = resp;
+          msg.title = "observation response";
+          const buf = Buffer.from(resp, 'base64');
+          let state = buf[3]; // TODO: parse TLV
+          msg.payload = state;
+          node.error(err);
+          node.send(msg);
+        });
+      }
+
+      //const PutRequest = node.device.put('/1/0/3', node.period, (data) => {
+    }
+  }
+  RED.nodes.registerType('sensor3700 in', SensorNode);
+};
