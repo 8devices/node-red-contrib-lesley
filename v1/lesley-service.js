25c46debbbba8e1713f09826e181920d8083e20f
Nedas Jarmolenka <nedas@8devices.com>
2018-03-16 18:13:06 +0000

    Notification pull flooding  after re-deploy fix *Added clear interval method which stops notification pulling after nodes are closed

--- v1/lesley-service.js
+++ v1/lesley-service.js
@@ -5,12 +5,14 @@ const restAPI = require('restserver-api');
 module.exports = function (RED) {
   function LesleyService(config) {
     RED.nodes.createNode(this, config);
-
-    const service = this;
-    service.options = {};
-    service.options.name = config.name;
-    service.options.url = config.url;
-    service.service = new restAPI.Service({ host: service.options.url });
+    this.options = {};
+    this.options.name = config.name;
+    this.options.url = config.url;
+    this.service = new restAPI.Service({ host: this.options.url });
   }
   RED.nodes.registerType('lesley-service', LesleyService);
+
+  LesleyService.prototype.close = function () {
+    clearInterval(this.service.pollTimer);
+  };
 };
