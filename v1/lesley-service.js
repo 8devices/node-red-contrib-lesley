cb455e1fc5516d175bf374f478dcafe24d068d19
Nedas Jarmolenka <nedas@8devices.com>
2018-05-04 15:27:35 +0000

    Service now supports both notification receiving methods (Callback register and notification pulling)

--- v1/lesley-service.js
+++ v1/lesley-service.js
@@ -10,7 +10,19 @@ module.exports = function (RED) {
     this.options.url = config.url;
     this.options.notificationMethod = config.notificationMethod;
     this.options.methodValue = config.methodValue;
-    this.service = new restAPI.Service({ host: this.options.url });
+    if (this.options.notificationMethod === 'callback') {
+      this.service = new restAPI.Service({
+        host: this.options.url,
+        polling: false,
+        port: this.options.methodValue,
+      });
+    } else if (this.options.notificationMethod === 'polling') {
+      this.service = new restAPI.Service({
+        host: this.options.url,
+        polling: true,
+        interval: this.options.methodValue,
+      });
+    }
     this.service.start();
   }
   RED.nodes.registerType('lesley-service', LesleyService);
