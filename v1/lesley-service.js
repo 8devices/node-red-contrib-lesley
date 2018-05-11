0c03a3115e761d9259b52003a7ab5a88bee7f0fe
Nedas <nedas@8devices.com>
2018-05-11 10:46:48 +0000

    Service update (#42)
    * lesley service update:
    *Added new fields (notification method and port/interval).
    
    * Service now supports both notification receiving methods (Callback register and notification pulling)
    
    * *Changed interval from miliseconds to seconds.
    *Added default values for method value.
    
    * Added Information of "lesley-service" node.
    
    * service html replaced spaces with tabs.
    
    * Service object creation after options has been handled
    
    * Replaced tabs with spaces
    
    * Created options object to configure restAPI service.

--- v1/lesley-service.js
+++ v1/lesley-service.js
@@ -5,10 +5,21 @@ const restAPI = require('restserver-api');
 module.exports = function (RED) {
   function LesleyService(config) {
     RED.nodes.createNode(this, config);
-    this.options = {};
-    this.options.name = config.name;
-    this.options.url = config.url;
-    this.service = new restAPI.Service({ host: this.options.url });
+    const options = {
+      host: 'http://localhost:8888',
+      interval: 1234,
+      polling: false,
+      port: 5728,
+    };
+    options.host = config.url;
+    if (config.notificationMethod === 'callback') {
+      options.polling = false;
+      options.port = config.methodValue;
+    } else if (config.notificationMethod === 'polling') {
+      options.polling = true;
+      options.interval = config.methodValue * 1000;
+    }
+    this.service = new restAPI.Service(options);
     this.service.start();
   }
   RED.nodes.registerType('lesley-service', LesleyService);
