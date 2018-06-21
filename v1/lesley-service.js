55a5f95f3c777b21856bc1ab6c26886d280d7dbf
Nedas Jarmolenka <nedas@8devices.com>
2018-06-21 16:58:45 +0300

    *Added ability to upload Ca Certificate file for secure connection.

--- v1/lesley-service.js
+++ v1/lesley-service.js
@@ -5,24 +5,55 @@ const restAPI = require('restserver-api');
 module.exports = function (RED) {
   function LesleyService(config) {
     RED.nodes.createNode(this, config);
-    const options = {
+    
+    const serviceOptions = {
       host: 'http://localhost:8888',
+      options: {
+        host: 'localhost',
+        port: '8888',
+        path: '/',
+        ca: '',
+      },
+      https: false,
       interval: 1234,
       polling: false,
       port: 5728,
     };
-    options.host = config.url;
-    if (config.notificationMethod === 'callback') {
-      options.polling = false;
-      options.port = config.methodValue;
-    } else if (config.notificationMethod === 'polling') {
-      options.polling = true;
-      options.interval = config.methodValue * 1000;
+    
+    let url = config.url;
+    if ((url.indexOf('http://') !== 0) && (url.indexOf('https://') !== 0)) {
+      if (config.useCa) {
+        url = `https://${url}`;
+      } else {
+        url = `http://${url}`;
+      }
     }
-    this.service = new restAPI.Service(options);
+    if (url.indexOf('http://') === 0) {
+      serviceOptions.https = false;
+      serviceOptions.host = url;
+    } else if (url.indexOf('https://') === 0) {
+      serviceOptions.https = true;
+      const hostAddress = url.slice('https://'.length).split(':');
+      console.log(hostAddress);
+      serviceOptions.options.host = hostAddress[0];
+      serviceOptions.options.port = hostAddress[1];
+      serviceOptions.options.ca = this.credentials.cadata;
+    }
+    if (config.notificationMethod === 'callback') {
+      serviceOptions.polling = false;
+      serviceOptions.port = config.methodValue;
+    } else if (config.notificationMethod === 'polling') {
+      serviceOptions.polling = true;
+      serviceOptions.interval = config.methodValue * 1000;
+    }
+    this.service = new restAPI.Service(serviceOptions);
     this.service.start();
   }
-  RED.nodes.registerType('lesley-service', LesleyService);
+  RED.nodes.registerType('lesley-service', LesleyService, {
+    credentials: {
+      cadata: { type: 'text' },
+    },
+  });
 
   LesleyService.prototype.close = function () {
     this.service.stop();
