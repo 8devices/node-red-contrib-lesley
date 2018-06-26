bd6afcdb648dec38a4617c9b4748804b04cc83d6
Nedas Jarmolenka <nedas@8devices.com>
2018-06-26 17:00:49 +0300

    *ESLint format

--- v1/sensor5200.js
+++ v1/sensor5200.js
@@ -16,7 +16,7 @@ module.exports = function (RED) {
     node.device = new restAPI.Device(node.service.service, node.name);
     node.state = false;
     node.cache = {};
-	
+
     function observe(resourcePath, resourceName, resourceType) {
       node.device.observe(resourcePath, (err, response) => {
         const msg = {};
@@ -63,89 +63,87 @@ module.exports = function (RED) {
       if (config.humidityBME) {
         observe('/3304/2/5700', 'humidityBME', RESOURCE_TYPE.FLOAT);
       }
-      
+
       if (config.pressureBME) {
         observe('/3315/2/5700', 'pressureBME', RESOURCE_TYPE.FLOAT);
       }
-      
+
       if (config.gasBME) {
         observe('/3327/2/5700', 'gasBME', RESOURCE_TYPE.FLOAT);
       }
-      
+
       if (node.accelerometer) {
-		  if (node.accelerometerX) {
-			observe('/3313/0/5702', 'accelerometerX', RESOURCE_TYPE.FLOAT);
-		  }
-		  
-		  if (node.accelerometerY) {
-			observe('/3313/0/5703', 'accelerometerY', RESOURCE_TYPE.FLOAT);
-		  }
-		  
-		  if (node.accelerometerZ) {
-			observe('/3313/0/5704', 'accelerometerZ', RESOURCE_TYPE.FLOAT);
-		  }
-	  }
-	  
+        if (node.accelerometerX) {
+          observe('/3313/0/5702', 'accelerometerX', RESOURCE_TYPE.FLOAT);
+        }
+
+        if (node.accelerometerY) {
+          observe('/3313/0/5703', 'accelerometerY', RESOURCE_TYPE.FLOAT);
+        }
+
+        if (node.accelerometerZ) {
+          observe('/3313/0/5704', 'accelerometerZ', RESOURCE_TYPE.FLOAT);
+        }
+      }
+
       if (node.gyroscope) {
-		  if (node.gyroscopeX) {
-			observe('/3334/0/5702', 'gyroscopeX', RESOURCE_TYPE.FLOAT);
-		  }
-		  
-		  if (node.gyroscopeY) {
-			observe('/3334/0/5703', 'gyroscopeY', RESOURCE_TYPE.FLOAT);
-		  }
-		  
-		  if (node.gyroscopeZ) {
-			observe('/3334/0/5704', 'gyroscopeZ', RESOURCE_TYPE.FLOAT);
-		  }
-	  }
-	  
-	  if (node.magnetometer) {
-		  if (node.magnetometerX) {
-			observe('/3314/0/5702', 'magnetometerX', RESOURCE_TYPE.FLOAT);
-		  }
-		  
-		  if (node.magnetometerY) {
-			observe('/3314/0/5703', 'magnetometerY', RESOURCE_TYPE.FLOAT);
-		  }
-		  
-		  if (node.magnetometerZ) {
-			observe('/3314/0/5704', 'magnetometerZ', RESOURCE_TYPE.FLOAT);
-		  }
-	  }
-	  
-	  if (config.temperatureHDC) {
+        if (node.gyroscopeX) {
+          observe('/3334/0/5702', 'gyroscopeX', RESOURCE_TYPE.FLOAT);
+        }
+
+        if (node.gyroscopeY) {
+          observe('/3334/0/5703', 'gyroscopeY', RESOURCE_TYPE.FLOAT);
+        }
+
+        if (node.gyroscopeZ) {
+          observe('/3334/0/5704', 'gyroscopeZ', RESOURCE_TYPE.FLOAT);
+        }
+      }
+
+      if (node.magnetometer) {
+        if (node.magnetometerX) {
+          observe('/3314/0/5702', 'magnetometerX', RESOURCE_TYPE.FLOAT);
+        }
+
+        if (node.magnetometerY) {
+          observe('/3314/0/5703', 'magnetometerY', RESOURCE_TYPE.FLOAT);
+        }
+
+        if (node.magnetometerZ) {
+          observe('/3314/0/5704', 'magnetometerZ', RESOURCE_TYPE.FLOAT);
+        }
+      }
+
+      if (config.temperatureHDC) {
         observe('/3303/1/5700', 'temperatureHDC', RESOURCE_TYPE.FLOAT);
       }
 
       if (config.humidityHDC) {
         observe('/3304/1/5700', 'humidityHDC', RESOURCE_TYPE.FLOAT);
       }
-      
+
       if (config.illuminance) {
         observe('/3301/0/5700', 'illuminance', RESOURCE_TYPE.FLOAT);
       }
     }
 
     node.on('input', (msg) => {
-		if(typeof msg.payload === 'string' || typeof msg.payload === 'number'){
-		let argument;
-		if(typeof msg.payload === 'string'){
-			argument = msg.payload;
-		}
-		else if (typeof msg.payload === 'number'){
-			argument = msg.payload.toString();
-		}
-		  node.device.write('/3341/0/5527', () => {
-		  }, encodeResource({
-			identifier: 5527,
-			type: RESOURCE_TYPE.STRING,
-			value: argument,
-		  }));
-		}
-		else{
-			node.error('Input message payload should be a number or a string');
-		}
+      if (typeof msg.payload === 'string' || typeof msg.payload === 'number') {
+        let argument;
+        if (typeof msg.payload === 'string') {
+          argument = msg.payload;
+        } else if (typeof msg.payload === 'number') {
+          argument = msg.payload.toString();
+        }
+        node.device.write('/3341/0/5527', () => {
+        }, encodeResource({
+          identifier: 5527,
+          type: RESOURCE_TYPE.STRING,
+          value: argument,
+        }));
+      } else {
+        node.error('Input message payload should be a number or a string');
+      }
     });
 
     node.device.on('register', () => {
@@ -198,7 +196,7 @@ module.exports = function (RED) {
       } else {
         node.error(err);
       }
-    }); 
+    });
   }
   RED.nodes.registerType('sensor5200 in', SensorNode);
 };
