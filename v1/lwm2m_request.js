8aa28b59343fad6c47ecc6db2828c757ebaabd14
Nedas <nedas@8devices.com>
2018-07-09 15:46:43 +0300

    Lwm2m request node observe operation (#47)
    * *Added observe operation in lwm2m request node.
    
    * *All functions in html file moved to the same script scope.
    *Integer check.
    *Observation cancel when closing the node.
    
    * Function name change for cancelling observations.
    
    * HTML file style fix.
    
    * *Changed observe interval value check.
    
    * *sensorNode.prototype.close is now declared before nodes registration.

--- v1/lwm2m_request.js
+++ v1/lwm2m_request.js
@@ -10,15 +10,14 @@ module.exports = function (RED) {
     RED.nodes.createNode(this, config);
     const node = this;
     node.service = RED.nodes.getNode(config.service);
-
     node.name = config.uuid;
+    node.device = new restAPI.Device(node.service.service, node.name);
     node.requestType = config.requestType;
     node.resourcePath = config.uri;
     node.resourceType = config.resourceType;
     node.inputValue = config.resourceValue;
     node.valueSource = config.valueSource;
-
-    node.device = new restAPI.Device(node.service.service, node.name);
+    node.observeInterval = config.observeInterval;
 
     let resourceType;
     switch (node.requestType) {
@@ -176,9 +175,78 @@ module.exports = function (RED) {
         break;
       }
 
+      case 'observe': {
+        if (!Number.isNaN(node.observeInterval)) {
+          node.device.write('/1/0/3', () => {
+          }, encodeResource({
+            identifier: 3,
+            type: RESOURCE_TYPE.INTEGER,
+            value: Math.ceil(Number(node.observeInterval)),
+          }));
+
+          node.on('input', () => {
+            if (node.resourcePath.split('/').length === 4) {
+              const resourceIdentifier = Number(node.resourcePath.split('/')[3]);
+
+              node.device.observe(node.resourcePath, (statusCode, payload) => {
+                node.observeStarted = true;
+                const buffer = Buffer.from(payload, 'base64');
+                const msg = {};
+
+                switch (node.resourceType) {
+                  case 'integer':
+                  case 'float':
+                  case 'string':
+                  case 'boolean':
+                  case 'opaque':
+                    resourceType = RESOURCE_TYPE[node.resourceType.toUpperCase()];
+                    break;
+                  default:
+                    resourceType = RESOURCE_TYPE.STRING;
+                }
+
+                const decodedResource = decodeResource(buffer, {
+                  type: resourceType,
+                  identifier: resourceIdentifier,
+                });
+
+                msg.payload = {};
+                msg.payload.uuid = node.name;
+                msg.payload.path = node.resourcePath;
+                msg.payload.statusCode = statusCode;
+                msg.payload.value = decodedResource.value;
+
+                node.send(msg);
+              }).catch((err) => {
+                if (typeof err === 'number') {
+                  node.error(`Error code: ${err}`);
+                } else {
+                  node.error(err);
+                }
+              });
+            } else {
+              node.error('Invalid path to resource. Must be "/object/instance/resource", e.g., "/1/0/3".');
+            }
+          });
+        } else {
+          node.error('Invalid observation interval. Must be an integer, e.g., "1", "2", "3".');
+        }
+
+        break;
+      }
+
       default:
         node.error('Unknown LwM2M request type.');
     }
   }
+
+  SensorNode.prototype.close = function () {
+    if (this.requestType === 'observe' && this.observeStarted) {
+      this.device.cancelObserve(this.resourcePath).catch((err) => {
+        this.error(`Error stopping observation: ${err}`);
+      });
+    }
+  };
+
   RED.nodes.registerType('LwM2M request in', SensorNode);
 };
