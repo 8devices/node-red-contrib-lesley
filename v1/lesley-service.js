6cf81bb9fc473de3968a8ab1fff6c4d414b9a3c3
Nedas <nedas@8devices.com>
2018-08-02 07:39:18 +0000

    Add HTTPS and JWT configurations to service node (#56)

--- v1/lesley-service.js
+++ v1/lesley-service.js
@@ -5,22 +5,55 @@ const restAPI = require('restserver-api');
 module.exports = function (RED) {
   function LesleyService(config) {
     RED.nodes.createNode(this, config);
-    const options = {
+
+    const serviceOptions = {
       host: 'http://localhost:8888',
+      ca: '',
+      authentication: false,
+      username: '',
+      password: '',
       interval: 1234,
       polling: false,
       port: 5728,
     };
-    options.host = config.url;
-    if (config.notificationMethod === 'callback') {
-      options.polling = false;
-      options.port = config.methodValue;
-    } else if (config.notificationMethod === 'polling') {
-      options.polling = true;
-      options.interval = config.methodValue * 1000;
+
+    let { url } = config;
+    if (!url.startsWith('http://') && !url.startsWith('https://')) {
+      if (config.useCa) {
+        url = `https://${url}`;
+      } else {
+        url = `http://${url}`;
+      }
     }
-    this.service = new restAPI.Service(options);
-    this.service.start();
+
+    serviceOptions.host = url;
+
+    if (config.useCa) {
+      serviceOptions.ca = this.credentials.cadata;
+    }
+
+    if (config.useAuthentication) {
+      serviceOptions.authentication = true;
+      serviceOptions.username = this.credentials.user;
+      serviceOptions.password = this.credentials.password;
+    }
+
+    if (config.notificationMethod === 'callback') {
+      serviceOptions.polling = false;
+      serviceOptions.port = config.methodValue;
+    } else if (config.notificationMethod === 'polling') {
+      serviceOptions.polling = true;
+      serviceOptions.interval = config.methodValue * 1000;
+    }
+
+    this.service = new restAPI.Service(serviceOptions);
+    this.service.start()
+      .then(() => {
+        this.emit('started');
+      })
+      .catch((err) => {
+        this.error(err);
+      });
 
     this.on('close', (done) => {
       this.service.stop().then(() => {
@@ -31,5 +64,11 @@ module.exports = function (RED) {
       });
     });
   }
-  RED.nodes.registerType('lesley-service', LesleyService);
+  RED.nodes.registerType('lesley-service', LesleyService, {
+    credentials: {
+      cadata: { type: 'text' },
+      user: { type: 'text' },
+      password: { type: 'password' },
+    },
+  });
 };
