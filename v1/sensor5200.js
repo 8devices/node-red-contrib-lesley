56e5710b46f1926e311ed5ff5ac760aa246271c9
mrNedas <nedas@8devices.com>
2018-08-07 14:51:50 +0000

    Added sensor5200 node (with battery voltage resource).

--- v1/sensor5200.js
+++ v1/sensor5200.js
@@ -1,24 +1,24 @@
 'use strict';
 
 const restAPI = require('restserver-api');
+const sensorMethods = require('./sensorMethods.js');
 
 const { Lwm2m } = restAPI;
-const { RESOURCE_TYPE, encodeResource, decodeResource } = Lwm2m.TLV;
+const { RESOURCE_TYPE, encodeResource } = Lwm2m.TLV;
 
 module.exports = function (RED) {
   function SensorNode(config) {
     RED.nodes.createNode(this, config);
     const node = this;
     node.service = RED.nodes.getNode(config.service);
-    node.service.attach(node);
 
-    node.observationInterval = Number(config.interval);
-    node.name = config.uuid;
-    node.paths = [];
-    node.device = new restAPI.Device(node.service.service, node.name);
-    node.state = false;
-    node.cache = {};
     node.resources = [
+      {
+        name: 'powerSourceVoltage', path: '/3/0/7', type: RESOURCE_TYPE.INTEGER, need: config.powerSourceVoltage,
+      },
+      {
+        name: 'screenText', path: '/3341/0/5527', type: RESOURCE_TYPE.STRING, need: config.screenText,
+      },
       {
         name: 'screenText', path: '/3341/0/5527', type: RESOURCE_TYPE.STRING, need: config.screenText,
       },
@@ -71,71 +71,9 @@ module.exports = function (RED) {
         name: 'illuminance', path: '/3301/0/5700', type: RESOURCE_TYPE.FLOAT, need: config.illuminance,
       },
     ];
-
-    function setStatus(state) {
-      if (state) {
-        node.status({ fill: 'green', shape: 'dot', text: 'connected' });
-      } else if (state !== undefined && !state) {
-        node.status({ fill: 'red', shape: 'dot', text: 'disconnected' });
-      } else if (!state) {
-        node.status({ fill: 'grey', shape: 'dot', text: 'unknown' });
-      }
-    }
-
-    function observe(resourcePath, resourceName, resourceType) {
-      node.device.observe(resourcePath, (err, response) => {
-        const msg = {};
-        const buffer = Buffer.from(response, 'base64');
-        const decodedResource = decodeResource(buffer, {
-          identifier: Number(resourcePath.split('/')[3]),
-          type: resourceType,
-        });
-        const resourceValue = decodedResource.value;
-        msg.payload = {
-          state: node.state,
-          data: {},
-        };
-        msg.payload.data[resourceName] = resourceValue;
-        node.cache[resourceName] = resourceValue;
-        msg.payload.cache = node.cache;
-        node.send(msg);
-      }).then((resp) => {
-        for (let i = 0; i < node.resources.length; i += 1) {
-          if (node.resources[i].name === resourceName) {
-            node.resources[i].observeAsyncID = resp;
-          }
-        }
-      }).catch((err) => {
-        if (typeof err === 'number') {
-          node.error(`Error starting observation, code: ${err}`);
-        } else {
-          node.error(err);
-        }
-      });
-    }
-
-    function configure() {
-      node.device.write('/1/0/3', () => {
-      }, encodeResource({
-        identifier: 3,
-        type: RESOURCE_TYPE.INTEGER,
-        value: node.observationInterval,
-      })).then(() => {
-        node.resources.forEach((resource) => {
-          if (resource.need) {
-            observe(resource.path, resource.name, resource.type);
-          }
-        });
-      }).catch((err) => {
-        if (typeof err === 'number') {
-          node.error(`Error setting observation time interval, code: ${err}`);
-        } else {
-          node.error(err);
-        }
-      });
-    }
-
-    setStatus();
+    sensorMethods.initialize(node, config);
+    sensorMethods.setStatus(node);
+    sensorMethods.registerEvents(node);
 
     node.on('input', (msg) => {
       if (typeof msg.payload === 'string' || typeof msg.payload === 'number') {
@@ -149,82 +87,6 @@ module.exports = function (RED) {
       } else {
         node.error('Input message payload should be a number or a string');
       }
-    });
-
-    node.device.on('register', () => {
-      const msg = {};
-      msg.payload = {};
-      node.state = true;
-      setStatus(node.state);
-      msg.payload.state = node.state;
-      msg.payload.data = {};
-      msg.payload.cache = node.cache;
-      node.send(msg);
-      configure();
-    });
-
-    node.device.on('update', () => {
-      node.state = true;
-      setStatus(node.state);
-    });
-
-    node.device.on('deregister', () => {
-      const msg = {};
-      msg.payload = {};
-      node.state = false;
-      setStatus(node.state);
-      msg.payload.state = node.state;
-      msg.payload.data = {};
-      msg.payload.cache = node.cache;
-      node.send(msg);
-    });
-
-    node.service.on('started', () => {
-      node.device.getObjects().then(() => {
-        const msg = {};
-        msg.payload = {};
-        node.state = true;
-        setStatus(node.state);
-        msg.payload.state = node.state;
-        msg.payload.data = {};
-        msg.payload.cache = node.cache;
-        node.send(msg);
-        configure();
-      }).catch((err) => {
-        if (typeof err === 'number') {
-          if (err === 404) {
-            const msg = {};
-            msg.payload = {};
-            node.state = false;
-            setStatus(node.state);
-            msg.payload.state = node.state;
-            msg.payload.data = {};
-            msg.payload.cache = node.cache;
-            node.send(msg);
-          } else {
-            node.error(`Error getting objects for endpoint, code: ${err}`);
-          }
-        } else {
-          node.error(err);
-        }
-      });
-    });
-
-    node.on('close', (done) => {
-      const cancelObservationPromises = [];
-
-      for (let i = 0; i < node.resources.length; i += 1) {
-        if (node.resources[i].observeAsyncID !== undefined) {
-          cancelObservationPromises.push(node.device.cancelObserve(node.resources[i].path));
-        }
-      }
-
-      Promise.all(cancelObservationPromises).catch((err) => {
-        node.error(err);
-      }).finally(() => {
-        node.service.detach(node);
-        done();
-      });
     });
   }
 
